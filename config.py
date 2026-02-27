885f16f47a817549431029ac3b1369191c6b27f4
jack2w2 <allenwu3389@gmail.com>
2026-02-28 01:36:45 +0800

    Update config.py
    更新获取

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 # 从环境变量读取配置，如果没有则使用默认值
 POLYMARKET_CONFIG = {
-    "FUNDER_ADDRESS": "",
-    "PRIVATE_KEY": "",
+    "FUNDER_ADDRESS": "0x0bd6b78d5953d2e11d6f1d00fb8ca33a836047a0",
+    "PRIVATE_KEY": "0x053f86336fb7558eb89dc01159dfccccde1d658f464acefb1ad0972b7fe5b7d1",
     "SIGNATURE_TYPE": 2
 }
 # Signature types:
